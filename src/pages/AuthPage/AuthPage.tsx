7c264f776270cf6d00eb87da4fc3b66dce37d123
Riya Jain <jriya.shi@gmail.com>
2023-05-28 15:32:33 +0530

    Navbar hide functionality

---
+++ src/pages/AuthPage/AuthPage.tsx
@@ -0,0 +1,11 @@
+export default function AuthPage() {
+    return <div className="flex flex-row h-">
+    <div className="basis-1/3 bg-primary">
+okay
+    </div>
+    <div>
+not okay
+    </div>
+    </div>;
+  }
+  
