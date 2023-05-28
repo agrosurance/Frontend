f9fbb3d87af1dba146ece74866583c8f940b5d8a
Riya Jain <jriya.shi@gmail.com>
2023-05-28 16:51:03 +0530

    Auth page ui completed

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
+import Banner from "./components/Banner";
+import Form from "./components/Form";
+
 export default function AuthPage() {
-    return <div className="flex flex-row h-">
-    <div className="basis-1/3 bg-primary">
-okay
-    </div>
-    <div>
-not okay
-    </div>
-    </div>;
-  }
-  
+  const [animFlag, setAnimFlag] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimFlag(true);
+    }, 200);
+  }, []);
+
+  return (
+    <>
+      <div className="absolute top-0 left-0 w-full h-full -z-1 bg-primary brightness-[83%]"></div>
+      <div className="flex flex-row h-screen overflow-hidden">
+        <Banner className={animFlag ? "translate-x-0" : "-translate-x-full"} />
+        <Form className={animFlag ? "translate-x-0" : "translate-x-full"} />
+      </div>
+    </>
+  );
+}
