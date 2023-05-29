d7eedeb3c8c75bedae164bfa5eb131fc21982fdf
Riya Jain <jriya.shi@gmail.com>
2023-05-30 00:05:46 +0530

    Staking modal in progress

--- src/pages/StakingPage/StakingPage.tsx
+++ src/pages/StakingPage/StakingPage.tsx
@@ -48,19 +48,32 @@ export default function StakingPage() {
             </button>
           </div>
         </div>
-        <div className="m-20 w-[33%] p-10 flex flex-col rounded-lg border border-primary gap-y-6">
+        <div className="m-20 w-[35vw] p-10 flex flex-col rounded-xl gap-y-6">
           <h1 className="self-center text-2xl font-bold text-primary">
             Overview
           </h1>
           <div>
             <h4 className="text-md font-medium">Stakes</h4>
             <div className="flex flex-row items-center border-front border border-opacity-50 rounded-xl">
-              <span className="text-4xl py-2 px-2 border border-r-front"></span>
+              <span className="text-4xl py-2 px-2 border border-r-front">
+                <img src="/logo.png" alt="logo" className="w-[1.5ch]" />
+              </span>
               <input
                 type="number"
                 placeholder="Enter the number of stakes"
                 className="text-md px-2 w-full"
               />
+            </div>
+          </div>
+          <h6>You will recieve:</h6>
+          <div className="flex flex-row gap-x-8 justify-center">
+            <div className="border-4 border-primary text-front px-5 py-1 rounded-full aspect-square flex flex-col justify-center items-center">
+              <span className="font-bold text-3xl">23</span>
+              <span>L-matic</span>
+            </div>
+            <div className="border-4 border-primary text-front py-1 px-2 rounded-full aspect-square flex flex-col justify-center items-center">
+              <span className="font-bold text-3xl">67</span>
+              <span>Agro-coins</span>
             </div>
           </div>
         </div>
