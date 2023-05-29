efde0d181f973163e587ff50d0a96a8f63c9d05a
Riya Jain <jriya.shi@gmail.com>
2023-05-30 00:36:35 +0530

    Strucked at some error in stake modal

--- src/pages/StakingPage/components/StakingModal.tsx
+++ src/pages/StakingPage/components/StakingModal.tsx
@@ -1,6 +1,20 @@
+import { useEffect, useRef, useState } from "react";
+import useModal from "../../../hooks/useModal";
+
 export default function StakingModal() {
+  const stakeRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+  const modal = useModal();
+
   return (
-    <div className="w-[33%] px-10 py-10 flex flex-col rounded-2xl gap-y-8 bg-back">
+    <div className="w-[33%] px-10 py-10 flex flex-col rounded-2xl gap-y-8 bg-back relative">
+      <button
+        className="absolute right-10"
+        onClick={() => {
+          modal.hide();
+        }}
+      >
+        <span className="material-icons">&#xe5cd;</span>
+      </button>
       <h1 className="self-center text-2xl font-bold text-primary">Overview</h1>
       <div>
         <h4 className="text-md font-medium">Stakes</h4>
@@ -12,6 +26,8 @@ export default function StakingModal() {
             type="number"
             placeholder="Enter the number of stakes"
             className="text-md px-2 w-full"
+            step={0.0001}
+            ref={stakeRef}
           />
         </div>
       </div>
@@ -19,11 +35,11 @@ export default function StakingModal() {
         <h6>You will recieve:</h6>
         <div className="flex flex-row gap-x-8 justify-center">
           <div className="border-4 border-primary text-front px-5 py-1 rounded-full aspect-square flex flex-col justify-center items-center">
-            <span className="font-bold text-3xl">23</span>
+            <span className="font-bold text-3xl">{stakeRef.current.value}</span>
             <span>L-matic</span>
           </div>
           <div className="border-4 border-primary text-front py-1 px-2 rounded-full aspect-square flex flex-col justify-center items-center">
-            <span className="font-bold text-3xl">67</span>
+            <span className="font-bold text-3xl">{stakeRef.current.value}</span>
             <span>Agro-coins</span>
           </div>
         </div>
