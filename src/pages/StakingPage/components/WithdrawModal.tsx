838cbc0bea2bd8f747553ca8d58c9107fcbe0eb1
Riya Jain <jriya.shi@gmail.com>
2023-05-30 17:04:30 +0530

    Incomplete WithdrawModal

--- src/pages/StakingPage/components/WithdrawModal.tsx
+++ src/pages/StakingPage/components/WithdrawModal.tsx
@@ -1,6 +1,25 @@
 import { useState } from "react";
 
 export default function WithdrawModal() {
-  const [loading, setLoading] = useState(true);
-  return <div className="bg-back"></div>;
+  const [loading, setLoading] = useState(false);
+  const [enable, setEnable] = useState(false);
+  return (
+    <div className="bg-back min-w-[30%] py-20 rounded-2xl px-20 ">
+      {loading ? (
+        <div className="flex flex-col items-center ">
+          <img src="/loading.gif" alt="loading" className="h-48" />
+          <p className="text-5xl font-bold text-front">Loading...</p>
+        </div>
+      ) : enable ? (
+        <div>hAHA</div>
+      ) : (
+        <div className="flex flex-col items-center">
+          <p className="text-xl text-center text-red-500">
+            <span className="text-2xl font-bold">Uhh ohh,</span>
+            <br /> You can't withdraw at this moment
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
