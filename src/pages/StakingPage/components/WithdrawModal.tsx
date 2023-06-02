f83891bfce57a39a906866d3cc183b663d7a3f7d
Riya Jain <jriya.shi@gmail.com>
2023-06-02 13:46:13 +0530

    withdraw modules and modals

--- src/pages/StakingPage/components/WithdrawModal.tsx
+++ src/pages/StakingPage/components/WithdrawModal.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import useFetch from "../../../hooks/useFetch";
 import MaterialIcon from "../../../common/MaterialIcon";
 import useModal from "../../../hooks/useModal";
+import { clamp } from "../../../utils";
 
 export default function WithdrawModal() {
   const [enable, loading] = useFetch<{ enabled: boolean }>("/dummy.json", {
@@ -16,7 +17,7 @@ export default function WithdrawModal() {
       ) : !enable.enabled ? (
         <DisabledState />
       ) : (
-        <div>hAHA</div>
+        <WithdrawState />
       )}
     </div>
   );
@@ -51,6 +52,61 @@ function DisabledState() {
           <p>Unfortunately, you can't withdraw right now</p>
           <p>Don't worry, your MATIC is safe with us</p>
           <p>meanwhite you can use your LMATIC</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function WithdrawState() {
+  const modal = useModal();
+
+  const [amount, setAmount] = useState<number>(0);
+
+  return (
+    <div className="">
+      <button
+        className="absolute right-6 top-6 text-white duration-300 scale-110 hover:scale-125"
+        onClick={modal.hide}
+      >
+        <MaterialIcon codepoint="e5cd" />
+      </button>
+      <h2 className="text-4xl bg-secondary py-10 text-white font-bold font-raleway tracking-tighter text-center mb-10">
+        Withdraw?
+      </h2>
+      <div className="px-6 flex flex-col justify-center">
+        <div className="flex flex-row gap-x-4 items-center">
+          <div className="flex flex-row items-center border-front border rounded-xl">
+            <MaterialIcon
+              codepoint="e8fb"
+              className="scale-125 text-4xl self-center text-secondary w-max border border-r-front px-1  border-transparent"
+            />
+            <input
+              type="number"
+              placeholder="Enter the number of stakes"
+              className="text-md px-4 w-[90%]"
+              step={0.00001}
+              value={amount}
+              onChange={(e) => {
+                setAmount(clamp(Number(e.target.value), { min: 0, max: 9999 }));
+              }}
+            />
+            <span className="border border-l-front px-3 border-transparent whitespace-nowrap py-3">
+              L-Matic
+            </span>
+          </div>
+          <button className="bg-secondary px-2 py-3 rounded-xl text-md text-back font-semibold duration-300 hover:brightness-110 hover:-translate-y-1">
+            Claim
+          </button>
+        </div>
+        <div className="flex flex-row justify-around py-6">
+          <div className="flex flex-col items-center gap-y-2">
+            <div className="text-xl font-semibold">You will get</div>
+            <div className="border-4 border-secondary text-front py-1 px-2 rounded-full aspect-square flex flex-col justify-center items-center">
+              <span className="font-bold text-3xl">{amount.toFixed(4)}</span>
+              <span>Matic</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
