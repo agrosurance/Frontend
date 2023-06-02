18e2f5682ec44e567238cb36d9aa507bac162dca
Riya Jain <jriya.shi@gmail.com>
2023-06-02 12:05:08 +0530

    Material-icon shortcut

--- src/pages/StakingPage/components/WithdrawModal.tsx
+++ src/pages/StakingPage/components/WithdrawModal.tsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import useFetch from "../../../hooks/useFetch";
+import MaterialIcon from "../../../common/MaterialIcon";
 
 export default function WithdrawModal() {
-  const [loading, setLoading] = useState(false);
-  const [enable, setEnable] = useState(false);
+  const [enable, loading] = useFetch<{ enabled: boolean }>(
+    "https://api.npoint.io/16919b31a418895a7b42",
+    { initial: { enabled: false }, cacheTimeout: 1 }
+  );
+
   return (
     <div className="bg-back min-w-[30%] py-20 rounded-2xl px-20 ">
       {loading ? (
@@ -10,10 +15,11 @@ export default function WithdrawModal() {
           <img src="/loading.gif" alt="loading" className="h-48" />
           <p className="text-5xl font-bold text-front">Loading...</p>
         </div>
-      ) : enable ? (
+      ) : enable.enabled ? (
         <div>hAHA</div>
       ) : (
         <div className="flex flex-col items-center">
+          <MaterialIcon codepoint="e811" />
           <p className="text-xl text-center text-red-500">
             <span className="text-2xl font-bold">Uhh ohh,</span>
             <br /> You can't withdraw at this moment
