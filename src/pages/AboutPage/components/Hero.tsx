44a1b4220909842f51cb15adfdcd8d9a786abd60
Spandan Barve <spandan567@gmail.com>
2023-06-03 17:30:40 +0530

    About pag eHero

--- src/pages/AboutPage/components/Hero.tsx
+++ src/pages/AboutPage/components/Hero.tsx
@@ -1,12 +1,35 @@
 export default function Hero() {
   return (
-    <section className="p-page flex">
-      <video width="320" height="240" autoPlay={true} muted loop>
+    <section className="p-page flex items-center overflow-hidden">
+      <video
+        className="relative -z-1 basis-1/2 scale-125 object-cover"
+        autoPlay
+        muted
+        loop
+      >
         <source src="/videos/happy-farmer1.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-
-      <section className="p-page z-10 m-12 flex bg-red-500 before:bg-black"></section>
+      <div className="relative flex basis-1/2 flex-col items-center gap-y-8">
+        <div className="flex flex-col items-center gap-y-2 text-center font-raleway text-5xl font-semibold text-primary">
+          <h1 className="font-bold brightness-75">
+            First Farmer-First Initiative
+          </h1>
+          <h2 className="saturate-150">for provably fair rates on</h2>
+          <h2 className="saturate-150">farmland insurance</h2>
+        </div>
+        <p className="px-2 text-sm">
+          At AgroSurance, we are revolutionizing the way farmers protect their
+          crops. We understand the challenges and uncertainties that farmers
+          face when it comes to safeguarding their livelihoods, and we are
+          committed to providing innovative solutions through our decentralized
+          application (dApp) powered by Chainlink. <br /> <br /> Our mission is
+          simple: to provide farmers with transparent, reliable, and efficient
+          crop insurance. We believe that every farmer deserves access to
+          affordable insurance that is tailored to their specific needs and
+          backed by advanced technology.
+        </p>
+      </div>
     </section>
   );
 }
