418a99899b17f1692e3518fd12ada33d46f8f170
Riya Jain <jriya.shi@gmail.com>
2023-06-04 21:15:21 +0530

    MakeClaimModal completed frontend

--- src/pages/FarmerPage/components/modals/GetInsuranceModal.tsx
+++ src/pages/FarmerPage/components/modals/GetInsuranceModal.tsx
@@ -1,6 +1,97 @@
+import useModal from "../../../../hooks/useModal";
+import MaterialIcon from "../../../../common/MaterialIcon";
+import useFetch from "../../../../hooks/useFetch";
+
 interface GetInsuranceModalProps {
   landId: number;
 }
 export default function GetInsuranceModal(props: GetInsuranceModalProps) {
-  return <>Insurance kr na be</>;
+  const modal = useModal();
+  return (
+    <div className="relative min-w-[30%] overflow-hidden rounded-2xl bg-back">
+      <button
+        className="absolute right-6 top-6 scale-110 text-back duration-300 hover:scale-125"
+        onClick={modal.hide}
+      >
+        <MaterialIcon codepoint="e5cd" />
+      </button>
+
+      <h2 className="mb-10 bg-blue-500 py-10 text-center font-raleway text-4xl font-bold tracking-tighter text-white">
+        Get Insurance
+      </h2>
+
+      <InitialState />
+    </div>
+  );
+}
+
+function LoadingState() {
+  return (
+    <div className="mb-10 flex flex-col items-center gap-y-4 px-10">
+      <p>Your request is been processed, You may come back later.</p>
+      <button className="text-md flex w-[6ch] items-center justify-center rounded-xl border-2 border-blue-500 px-2 py-2 font-semibold text-blue-500 duration-300 hover:-translate-y-1 hover:brightness-110">
+        <MaterialIcon codepoint="e5d5" />
+      </button>
+    </div>
+  );
+}
+
+function InitialState() {
+  const modal = useModal();
+
+  return (
+    <div className="mb-10 flex flex-col gap-y-4 px-10">
+      <p>How much Insurance do you want to have for this peice of land?</p>
+      <div className=" flex flex-row justify-around">
+        <button className="text-md w-[6ch] rounded-xl bg-blue-500 px-2 py-2 font-semibold text-white duration-300 hover:-translate-y-1 hover:brightness-110">
+          Yes
+        </button>
+        <button
+          onClick={modal.hide}
+          className="text-md w-[6ch] rounded-xl border-2 border-blue-500 px-2 py-2 font-semibold text-blue-500 duration-300 hover:-translate-y-1 hover:brightness-110"
+        >
+          No
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function FailedState() {
+  return (
+    <div className="mb-10 flex flex-col gap-y-4 px-10">
+      <p>your request was declined, You can't make a claim right now.</p>
+      <div className=" flex flex-row justify-around">
+        <div className="flex gap-x-10">
+          <button className="text-md w-[6ch] rounded-xl bg-blue-500 px-2 py-2 font-semibold text-white duration-300 hover:-translate-y-1 hover:brightness-110">
+            Okay
+          </button>
+        </div>
+      </div>
+      <p className="w-full text-justify text-sm">
+        If you believe this was a mistake, you may raise a case against it.{" "}
+        <br />
+        our judge protocol will jump into action to ensure your issue is
+        resolved
+      </p>
+      <button className="mr-8 w-max self-end rounded-xl border-2 border-blue-500 px-4 py-1 text-sm text-blue-500 duration-300 hover:border-background hover:bg-blue-500 hover:text-white">
+        Report
+      </button>
+    </div>
+  );
+}
+
+function SuccessState() {
+  return (
+    <div className="flex flex-col items-center px-10">
+      <p>
+        You can successfully claim
+        <span className="mx-1 font-medium text-blue-500">{90}</span>
+        MATIC for this farmland
+      </p>
+      <button className="my-3 w-max rounded-lg bg-blue-500 px-6 py-2 text-white duration-300 hover:-translate-y-1 hover:saturate-150">
+        Claim
+      </button>
+    </div>
+  );
 }
