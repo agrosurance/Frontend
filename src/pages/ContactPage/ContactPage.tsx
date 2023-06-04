5838cabfd14721e617ed1a12a446a08784ba7877
Spandan Barve <spandan567@gmail.com>
2023-06-05 01:08:41 +0530

    contact page

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,3 +1,5 @@
+import MaterialIcon from "../../common/MaterialIcon";
+
 export default function ContactPage() {
   return (
     <>
@@ -10,7 +12,38 @@ export default function ContactPage() {
             Our team is here to assist you and provide the information you need.
             Please don't hesitate to reach out to us
           </p>
-          <form></form>
+          <form
+            className="my-12 flex flex-col gap-y-6"
+            action="https://formspree.io/f/xyyaqwlg"
+            method="POST"
+          >
+            <input
+              type="text"
+              className="w-1/3 rounded-lg bg-transparent px-4 py-2 text-red-950 outline outline-red-950"
+              placeholder="Your Name"
+              name="name"
+              required
+            />
+            <input
+              type="text"
+              className="w-1/3 rounded-lg bg-transparent px-4 py-2 text-red-950 outline outline-red-950"
+              placeholder="Your Email Address"
+              name="email"
+              required
+            />
+            <textarea
+              rows={6}
+              className="w-1/3 resize-none rounded-lg bg-transparent px-4 py-2 text-red-950 outline outline-red-950"
+              placeholder="Your Message"
+              name="message"
+              required
+            />
+            <div className="flex w-1/3 justify-center">
+              <button className="flex w-max items-center gap-x-2 rounded-md bg-red-950 px-6 py-1 text-white duration-300 hover:-translate-y-1 hover:scale-105 hover:shadow-lg">
+                Submit <MaterialIcon codepoint="e163" className="text-lg" />
+              </button>
+            </div>
+          </form>
         </div>
       </section>
     </>
