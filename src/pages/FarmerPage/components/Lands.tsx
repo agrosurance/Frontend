6d02e9490b181925cf6d8f812833fb2ff79b7f78
Spandan Barve <spandan567@gmail.com>
2023-06-07 13:30:00 +0530

    edge case - no lands

--- src/pages/FarmerPage/components/Lands.tsx
+++ src/pages/FarmerPage/components/Lands.tsx
@@ -4,69 +4,6 @@ import { useAuthContext } from "../../../contexts/AuthContext";
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 
-// const lands: Land[] = [
-//   {
-//     name: "bekaar zameen",
-//     insurance: {
-//       isInsured: false,
-//     },
-//     location: {
-//       latitude: 19.0601,
-//       longitude: 73.014,
-//     },
-//     area: 90,
-//   },
-//   {
-//     name: "Zameen e Virasat",
-//     crop: { name: "Carrot", imageUrl: "/images/lands/carrot.png" },
-//     insurance: {
-//       isInsured: true,
-//       insuredTill: Date.now() + 31234560000,
-//     },
-//     location: {
-//       latitude: 21.0601,
-//       longitude: 74.564,
-//     },
-//     area: 120,
-//   },
-//   {
-//     name: "Looti hui",
-//     crop: { name: "Corn", imageUrl: "/images/lands/corn.png" },
-//     insurance: {
-//       isInsured: true,
-//       insuredTill: Date.now() + 8122450000,
-//     },
-//     location: {
-//       latitude: 19.5601,
-//       longitude: 83.6147,
-//     },
-//     area: 20,
-//   },
-//   {
-//     name: "Idhar Farmhouse Banaundga",
-//     crop: { name: "Maize", imageUrl: "/images/lands/maize.png" },
-//     insurance: {
-//       isInsured: false,
-//     },
-//     location: {
-//       latitude: 53.0601,
-//       longitude: 13.014,
-//     },
-//     area: 5,
-//   },
-//   {
-//     name: "as dry as your girl",
-//     insurance: {
-//       isInsured: false,
-//     },
-//     location: {
-//       latitude: 49.0601,
-//       longitude: 23.014,
-//     },
-//     area: 100,
-//   },
-// ];
-
 export default function Lands() {
   const { agroSuranceLandContract, signer } = useAuthContext();
   const [lands, setLands] = useState<any[]>([]);
@@ -116,9 +53,13 @@ export default function Lands() {
       <h1 className="-mb-3 text-center font-raleway text-3xl font-bold tracking-tight">
         My Lands
       </h1>
-      {lands.map((land, i) => (
-        <LandCard land={land} key={i} landId={i} />
-      ))}
+      {lands.length ? (
+        lands.map((land, i) => <LandCard land={land} key={i} landId={i} />)
+      ) : (
+        <p className="mt-8 text-center text-lg italic text-front text-opacity-70">
+          You have no lands <br /> Lands you add will be listed here
+        </p>
+      )}
     </section>
   );
 }
